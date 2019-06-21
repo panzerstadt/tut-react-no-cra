9d7afe0bd6cc3f76fa3e3a0fbbeed856da8d0afb
panzerstadt <tliqun@gmail.com>
2019-06-21 16:45:43 +0900

    bundle version (no cdn)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const DynamicCdnWebpackPlugin = require("dynamic-cdn-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -36,7 +35,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html"
-    }),
-    new DynamicCdnWebpackPlugin()
+    })
   ]
 };
